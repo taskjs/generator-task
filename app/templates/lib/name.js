cc9932d0cd38637a7b232772b2e050ca296820f0
yuanyan <yuanyan.cao@gmail.com>
2014-03-28 19:26:12 +0800

    Update execution module to latest.

--- app/templates/lib/name.js
+++ app/templates/lib/name.js
@@ -1,20 +1,33 @@
 var Execution = require('execution');
 
 module.exports = Execution.extend({
-    name: '<%= shortSafeSlugname %>',
-    description: '<%= props.description %>',
-    // The type of option could be path, url, stream, array, string, number, date,
-    // and array of them like paths or urls that suffix with 's'.
+    // The type of option could be HTML5 input types: file, directory, number, range, select,
+    // url, email, tel, color, date, time, month, time, week, datetime(datetime-local),
+    // string(text), boolean(checkbox), array, function and object.
     options: {
-        paths: {
-            description: 'Files path',
-            type: 'paths'
+        foo: {
+            label: 'Amount of parallelism',
+            default: 5,
+            type: 'range',
+            min: 1,
+            max: 10
         },
-        encoding: {
-            description: 'File encoding',
+        bar: {
+            label: 'File encoding',
             default: 'utf8',
-            type: ['utf8', 'ascii', 'base64']
+            type: 'select',
+            options: ['utf8', 'ascii', 'base64']
+        },
+        baz: {
+            label: 'Paths',
+            type: 'file',
+            multiple: true,
+            accept: 'image/*',
+            placeholder: 'Try selecting more than one file when browsing for files.'
         }
+    },
+    run: function (inputs, options, logger) {
+        return this._run(inputs, options, logger);
     },
     execute: function (resolve, reject) {
         var options = this.options;
